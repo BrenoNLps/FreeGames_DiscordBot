a2e8d6e9fd9e3fa1d805dabfcf4279fb747deef7
brenonlps <brenolopes.xy@gmail.com>
2025-12-26 18:57:22 -0300

    feat: Initialize Discord.js client with gateway intents

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const {Client, GatewayIntentBits} = require('discord.js');
 
+const client = new Client({ intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent      
+] });
+
+
